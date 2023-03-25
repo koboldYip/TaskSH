ed1aaf1acea22be454f7815d0a670618d922a49a
Arthur Shmakotin <kyldoks@gmail.com>
2023-03-25 21:18:32 +0300

    Init commit

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,7 +1,9 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
+import hiber.service.CarService;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -9,26 +11,65 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class MainApp {
-   public static void main(String[] args) throws SQLException {
-      AnnotationConfigApplicationContext context = 
-            new AnnotationConfigApplicationContext(AppConfig.class);
+    public static void main(String[] args) throws SQLException {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
 
-      UserService userService = context.getBean(UserService.class);
+        UserService userService = context.getBean(UserService.class);
+        CarService carService = context.getBean(CarService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+        Car car1 = new Car("Tesla", 10);
+        Car car2 = new Car("BMW", 1);
+        Car car3 = new Car("Audi", 4);
+        Car car4 = new Car("Reno", 8);
 
-      List<User> users = userService.listUsers();
-      for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
-         System.out.println();
-      }
+        User user1 = new User("User1", "Lastname1", "user1@mail.ru");
+        User user2 = new User("User2", "Lastname2", "user2@mail.ru", car2);
+        User user3 = new User("User3", "Lastname3", "user3@mail.ru", car3);
+        User user4 = new User("User4", "Lastname4", "user4@mail.ru", car4);
 
-      context.close();
-   }
+        carService.add(car1);
+        carService.add(car2);
+        carService.add(car3);
+        carService.add(car4);
+
+        userService.add(user1);
+        userService.add(user2);
+        userService.add(user3);
+        userService.add(user4);
+
+        User userByCar = userService.getByCar(car1.getModel(), car1.getSeries());
+
+        System.out.println("Id = " + userByCar.getId());
+        System.out.println("First Name = " + userByCar.getFirstName());
+        System.out.println("Last Name = " + userByCar.getLastName());
+        System.out.println("Email = " + userByCar.getEmail());
+        if (userByCar.getCar() != null) {
+            System.out.println("Car model = " + userByCar.getCar().getModel());
+            System.out.println("Car series = " + userByCar.getCar().getSeries());
+        }
+        System.out.println();
+
+        List<User> users = userService.listUsers();
+        for (User user : users) {
+            System.out.println("Id = " + user.getId());
+            System.out.println("First Name = " + user.getFirstName());
+            System.out.println("Last Name = " + user.getLastName());
+            System.out.println("Email = " + user.getEmail());
+            if (user.getCar() != null) {
+                System.out.println("Car model = " + user.getCar().getModel());
+                System.out.println("Car series = " + user.getCar().getSeries());
+            }
+            System.out.println();
+        }
+
+        List<Car> cars = carService.listCars();
+        for (Car car : cars) {
+            System.out.println("Model = " + car.getModel());
+            System.out.println("Series = " + car.getSeries());
+            System.out.println();
+        }
+
+        context.close();
+    }
 }
--- 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
@@ -6,56 +6,71 @@ import javax.persistence.*;
 @Table(name = "users")
 public class User {
 
-   @Id
-   @GeneratedValue(strategy = GenerationType.IDENTITY)
-   private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+    @Column(name = "name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email")
+    private String email;
+    @OneToOne
+    @JoinColumn(name = "car_id")
+    private Car car;
 
-   @Column(name = "name")
-   private String firstName;
+    public User() {
+    }
 
-   @Column(name = "last_name")
-   private String lastName;
+    public User(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
 
-   @Column(name = "email")
-   private String email;
+    public User(String firstName, String lastName, String email, Car car) {
+        this(firstName, lastName, email);
+        this.car = car;
+    }
 
-   public User() {}
-   
-   public User(String firstName, String lastName, String email) {
-      this.firstName = firstName;
-      this.lastName = lastName;
-      this.email = email;
-   }
+    public Long getId() {
+        return id;
+    }
 
-   public Long getId() {
-      return id;
-   }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-   public void setId(Long id) {
-      this.id = id;
-   }
+    public String getFirstName() {
+        return firstName;
+    }
 
-   public String getFirstName() {
-      return firstName;
-   }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-   public void setFirstName(String firstName) {
-      this.firstName = firstName;
-   }
+    public String getLastName() {
+        return lastName;
+    }
 
-   public String getLastName() {
-      return lastName;
-   }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-   public void setLastName(String lastName) {
-      this.lastName = lastName;
-   }
+    public String getEmail() {
+        return email;
+    }
 
-   public String getEmail() {
-      return email;
-   }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-   public void setEmail(String email) {
-      this.email = email;
-   }
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
 }
